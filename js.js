db0731213d4f58d524f7735d15c381d6a784729e
MetatronL <paunbogdan97@windowslive.com>
2015-11-22 22:17:35 +0200

    15_nov_22___22_17

--- js.js
+++ js.js
@@ -15,18 +15,18 @@ function loadText(){
 
 function createEratosteneList(n){
 	var max_n =10;
-	var _this = document.getElementById("i_");	
+	var _this = document.getElementById("elem_container");	
 	var inv_n=2;
 	var code="";
 	for(var i=0;i*max_n<=n;++i){
 		code = "<div class='w3-row-padding' >";
-		$(_this).before(code);
+		document.getElementById("elem_container").innerHTML += code;
 		for(var j=max_n; j && inv_n <=n  ; --j, ++inv_n){
-				code = "<div id='dv_"+inv_n+"' class='w3-col  w3-center' style='width:9%;padding-right:10px;' ><p id='el_"+inv_n+"' class='border_1 w3-red ' >"+inv_n+"</p></div>";
-				$(_this).before(code);	
+				code = "<div id='dv_"+inv_n+"' class='w3-col  w3-center' style='width:10%;padding-right:10px;' ><p id='el_"+inv_n+"' class='border_1 w3-red ' >"+inv_n+"</p></div>";
+				document.getElementById("elem_container").innerHTML += code;	
 		}
 		code = "</div>";
-		$(_this).before(code);
+		document.getElementById("elem_container").innerHTML += code;
 	}
 
 }
@@ -38,7 +38,7 @@ function _clicked(){
 		else
 		document.getElementById("status").innerHTML = "" ;
 		document.getElementById("lista_finala").innerHTML = " <div id='__lista_finala' >&nbsp</div>"
-		document.getElementById("numbers_container").innerHTML = "<p id='i_' ></p>";
+		document.getElementById("elem_container").innerHTML = "";
 		createEratosteneList(n);
 		var lst = new Array(n+2);
 		for(var i=0;i<=n+1;++i) lst[i] = true;
